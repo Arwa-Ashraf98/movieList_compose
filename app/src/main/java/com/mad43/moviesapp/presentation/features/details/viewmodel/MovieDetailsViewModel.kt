21f07b28890346bc59a0fe9f417be2742d430556
arwa <arwashams643@gmail.com>
2024-05-19 14:01:54 +0300

    update movie details state in case of catch error

--- app/src/main/java/com/mad43/moviesapp/presentation/features/details/viewmodel/MovieDetailsViewModel.kt
+++ app/src/main/java/com/mad43/moviesapp/presentation/features/details/viewmodel/MovieDetailsViewModel.kt
@@ -8,6 +8,7 @@ import com.mad43.moviesapp.presentation.features.details.ui.MovieDetailsState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
@@ -16,7 +17,8 @@ import javax.inject.Inject
 
 @HiltViewModel
 class MovieDetailsViewModel @Inject constructor(
-    private val getMovieDetailsUseCase: GetMovieDetailsUseCase, ) : ViewModel() {
+    private val getMovieDetailsUseCase: GetMovieDetailsUseCase,
+) : ViewModel() {
 
 
     private var _detailsState = MutableStateFlow(MovieDetailsState())
@@ -25,27 +27,36 @@ class MovieDetailsViewModel @Inject constructor(
 
     fun getMovie(movieId: Int) {
         viewModelScope.launch {
-            _detailsState.update { it.copy(isLoading = true) }
-
-            getMovieDetailsUseCase(movieId = movieId).collectLatest { res ->
-                when (res) {
-                    is ResourceResult.SUCCESS -> {
-                        _detailsState.update {
-                            it.copy(isLoading = false, movie = res.data, error = null)
-                        }
+            _detailsState.update { it.copy(isLoading = true , error = null , movie = null) }
+            getMovieDetailsUseCase(movieId = movieId)
+                .catch {
+                    _detailsState.update {
+                        it.copy(
+                            isLoading = false,
+                            error = it.error,
+                            movie = null
+                        )
                     }
+                }
+                .collectLatest { res ->
+                    when (res) {
+                        is ResourceResult.SUCCESS -> {
+                            _detailsState.update {
+                                it.copy(isLoading = false, movie = res.data, error = null)
+                            }
+                        }
 
-                    is ResourceResult.ERROR -> {
-                        _detailsState.update {
-                            it.copy(
-                                isLoading = false,
-                                movie = null,
-                                error = res.error?.localizedMessage ?: "Something Went Wrong"
-                            )
+                        is ResourceResult.ERROR -> {
+                            _detailsState.update {
+                                it.copy(
+                                    isLoading = false,
+                                    movie = null,
+                                    error = res.error?.localizedMessage ?: "Something Went Wrong"
+                                )
+                            }
                         }
                     }
                 }
-            }
         }
     }
 }
